dda3531047d35efa6d84398928a12678eb46cf2a
Matheus <matheuscarretta@gmail.com>
2024-03-11 19:58:59 -0300

    cria rota com query params

--- src/app.js
+++ src/app.js
@@ -6,4 +6,20 @@ app.get('/', (req, res) => res.status(200).json(
   { message: "Olá mundo"}
 ));
 
+app.get("/query", (req, res) => {
+  console.log(req.query)
+
+
+  if ( !req.query.nome ){
+    return res.status(404).json(
+      { message: "Não foi possível encontrar a query nome"}
+    )
+  }
+
+ // http://localhost:3001/query?sobrenome=Pedro
+  return res.status(200).json(
+    { nome: req.query.nome, sobrenome: req.query.sobrenome }
+  )
+})
+
 module.exports = app;
