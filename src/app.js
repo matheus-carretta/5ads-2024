ffc4df235f62a3abdf05834bfcd796512681f764
Matheus <matheuscarretta@gmail.com>
2024-03-11 21:45:36 -0300

    adiciona rotas get, post e put

--- src/app.js
+++ src/app.js
@@ -2,9 +2,50 @@ const express = require('express');
 
 const app = express();
 
+const timesArray = [
+  {
+    id: 1,
+    nome: "Vasco",
+    sigla: "VAS"
+  },
+  {
+    id: 2,
+    nome: "Flamengo",
+    sigla: "FLA"
+  }
+]
+
+app.use(express.json());
+
+app.get("/teams", (req, res) => res.status(200).json(
+  { teams: timesArray}
+))
+
+app.post("/teams", (req, res) => {
+  const newTeam = req.body;
+  timesArray.push(newTeam)
+
+  return res.status(201).json({ team: newTeam })
+})
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { nome, sigla } = req.body;
+  const updateTeam = teams.find((team) => team.id === Number(id));
+  if (!updateTeam) {
+    return res.status(404).json({ message: 'Team not found' });
+  }
+  updateTeam.nome = nome;
+  updateTeam.sigla = sigla;
+  res.status(200).json({ updateTeam });
+});
+
+
+
 app.get('/', (req, res) => res.status(200).json(
   { message: "Olá mundo"}
 ));
+
 
 app.get("/query", (req, res) => {
   if ( !req.query.nome ){
@@ -23,6 +64,12 @@ app.get("/params/:id", (req, res) => {
   console.log(req.params)
 
   return res.status(200).json(req.params)
+})
+
+app.get("/body", (req, res) => {
+  console.log(req.body)
+
+  return res.status(200).json({ data: req.body })
 })
 
 module.exports = app;
